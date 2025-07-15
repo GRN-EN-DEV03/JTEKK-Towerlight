cdd643d838a4c3d702d06f6208ca75e11bd4bb8d
GRN-EN-DEV03 <nattawan.khamfubut@gmail.com>
2025-07-15 11:08:53 +0700

    Update : modbus coil control r/w

--- .src/Modbus/modbus_write.py
+++ .src/Modbus/modbus_write.py
@@ -26,63 +26,64 @@ def connect_to_modbus():
               "Please ensure the device is running and accessible.")
         return False
 
-def write_single_holding_register(address,value,MODBUS_SLAVE_ID):
+def write_coil_status(address,value,MODBUS_SLAVE_ID):
     """
-    Writes a single value to a single holding register.
+    Writes a single value to a single coils register.
 
     Args:
         address (int): The Modbus register address (0-based).
         value (int): The integer value to write to the register.
         MODBUS_SLAVE_ID : Meter ID or Device ID of modbus
     """
-    print(f"\nAttempting to write value {value} to holding register address {address}..")
+    print(f"\nAttempting to write value {value} to coils register address {address}..")
     try:
-        # Write the single holding register
+        # Write the single coils register
         # The 'unit' parameter is the slave ID
-        result = client.write_register(address, value, slave=MODBUS_SLAVE_ID)
+        result = client.write_coils(address-1, value, slave=MODBUS_SLAVE_ID)
         #print(result)
         if result.isError():
             print(f"Error writing single register: Modbus Exception - {result}")
         else:
-            print(f"Successfully wrote {value} to holding register {address}.")
+            print(f"Successfully wrote {value} to coils register {address}.")
     except Exception as e:
         print(f"An error occurred during single register write: {e}")
 
-def read_holding_register(address,MODBUS_SLAVE_ID) :
+def read_coil_status(address,MODBUS_SLAVE_ID) :
     """
-    Read a single value to a single holding register.
+    Read a single value to a single coils register.
 
     Args:
         address (int): The Modbus register address (0-based).
         MODBUS_SLAVE_ID : Meter ID or Device ID of modbus
     """
-    #print(f"\nAttempting to read value from holding register address {address}...")
+    #print(f"\nAttempting to read value from coils register address {address}...")
     try:
-        # Write the single holding register
+        # Write the single coils register
         # The 'unit' parameter is the slave ID
-        response = client.read_holding_registers(address,count=1 ,slave=MODBUS_SLAVE_ID)
-        datas = response.registers
+        #response = client.read_coils(1,count=10 ,slave=1)
+        response = client.read_coils(address-1,count=1 ,slave=MODBUS_SLAVE_ID)
+        datas = response.bits
         print("OUTPUT ----------------------------------------------------")
+        #print(response)
         print("Modbus Response : ",datas[0])
         print("-----------------------------------------------------------")
         if response.isError():
             print(f"Error reading single register: Modbus Exception - {response}")
             return 1
         else:
-            print(f"Successfully read from holding register {address}.")
+            print(f"Successfully read from coils register {address}.")
             return datas[0]
     except Exception as e:
         print(f"An error occurred during single register read: {e}")
-
 
 # --- Main execution block ---
 def main_write_register(address,value,MODBUS_SLAVE_ID) :
     # Ensure pymodbus is installed: pip install pymodbus
     if connect_to_modbus():
         # Write value 123 to register address 0
-        write_single_holding_register(address,value,MODBUS_SLAVE_ID)
+        write_coil_status(address,value,MODBUS_SLAVE_ID)
         time.sleep(0.1) # Small delay to ensure command completes
-        read_holding_register(address,MODBUS_SLAVE_ID)
+        read_coil_status(address,MODBUS_SLAVE_ID)
         # Close the Modbus connection when done
         client.close()
         print("\nModbus connection closed.")
